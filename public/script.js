4e39b6c679070608151cf318564fcba3fef92098
SabriLu11 <144378145+SabriLu11@users.noreply.github.com>
2024-07-07 14:09:47 -0300

    Finalizacion del FRONTEND

--- public/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', ()=>{
-    const createUserButton = document.getElementById('createUserButton');
-    const createUserForm = document.getElementById('createUserForm');
-    const editarUsuarioForm = document.getElementById ('editarUsuarioForm')
-    const listaUsuariosbtn = document.getElementById ('listaUsuariosbtn')
-    const listaUsuarios = document.getElementById
-
-})
